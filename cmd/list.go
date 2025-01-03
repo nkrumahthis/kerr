4b4e8bcb2bdaa93f095f5773918942e7910858b7
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2025-01-03 22:43:20 +0000

    list actions command

--- cmd/list.go
+++ cmd/list.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 	"nkrumahsarpong.com/kerr/core"
 )
@@ -13,15 +11,7 @@ var listCmd = &cobra.Command{
 	Long: `Show actionable items and reminders based on the book "How to Be Great at Your Job". 
 	Provide tags to filter principles, e.g., 'kerr list tasks' or 'kerr list meetings'.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		principles := core.GetPrinciples()
-
-		if len(args) == 0 {
-			fmt.Println("Things to focus on to do a great job:")
-			for i, principle := range principles {
-				fmt.Printf("%d. %s\n", i+1, principle.Description)
-			}
-			return
-		}
+		core.ListActions()
 	},
 }
 
